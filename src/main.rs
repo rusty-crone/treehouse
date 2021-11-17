cc501fe8f306cdc55a8ed5d1abc5d4fb2be24d7f
Todd W Crone <twcrone@gmail.com>
2021-11-17 08:57:21 -0500

    Add array stuff

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,19 @@ use std::io::stdin;
 fn main() {
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("Hello, {:?}", name);
+    let visitor_list = ["jason", "brad", "andr\u{e9}"];
+    let mut allow_them_in = false;
+    for visitor in &visitor_list {
+        if visitor == &name {
+            allow_them_in = true;
+        }
+    }
+
+    if allow_them_in {
+        println!("Hello, {:?}", name);
+    } else {
+        println!("You are not in the club {}", name);
+    }
 }
 
 fn what_is_your_name() -> String {
@@ -13,7 +25,5 @@ fn what_is_your_name() -> String {
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
-    your_name
-        .trim()
-        .to_lowercase()
+    your_name.trim().to_lowercase()
 }
